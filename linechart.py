e4d30de2dd63a340b48fa32cac7dfc55ed928b2d
John Madden <jmadden173@protonmail.com>
2022-07-14 09:02:16 -0700

    Added example for line chart

---
+++ linechart.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from bokeh.plotting import figure, show
+
+x = [1,2,3,4,5]
+y = [6,7,2,4,5]
+
+p = figure(title="Simple line example", x_axis_label='x', y_axis_label='y')
+p.line(x, y, legend_label="Temp.", line_width=2)
+
+show(p)
