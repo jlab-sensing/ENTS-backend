9ca29ad3c62b4343e289cb7580f6d57f5653d8b6
John Madden <jmadden173@protonmail.com>
2022-07-14 15:47:06 -0700

    Added static graphing of power

--- linechart.py
+++ linechart.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 
+import pdb
+
+import pandas as pd
 from bokeh.plotting import figure, show
 
-x = [1,2,3,4,5]
-y = [6,7,2,4,5]
+# Read soil date
+# NOTE: read_csv skips blank lines therefore the header index is 9
+# The data has two current channels and the I1L_valid and I2L_valid indicate
+# which channels is currently being used. For now only I*L are valid
+raw = pd.read_csv("soil_20220629-214516_8.csv", header=9,
+                        names=["timestamp", "I1L_valid", "I2L_valid", "I1H",
+                               "I1L", "V1", "V2", "I2H", "I2L"],
+                        )
+# Calculate timestamp and make index
+raw["timestamp"] = pd.to_datetime(raw["timestamp"], unit='s')
+raw = raw.set_index("timestamp")
+# Calculate power
+raw["P1"] = raw["V1"] * raw["I1L"]
+raw["P2"] = raw["V2"] * raw["I2L"]
 
-p = figure(title="Simple line example", x_axis_label='x', y_axis_label='y')
-p.line(x, y, legend_label="Temp.", line_width=2)
+# Calculate moving average
+moving_avg = raw.resample('10min').mean()
+
+p = figure(title="Simple line example", x_axis_label='time [s]',
+           y_axis_label='Power [Watts]')
+p.line(moving_avg.index, moving_avg["P1"], legend_label="Power", line_width=2)
 
 show(p)
