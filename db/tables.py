fb30fac0f56c57c49a78c1eebc85e568b03b0cce
John Madden <jmadden173@pm.me>
2022-07-25 15:14:03 -0700

    Added PowerData table

--- db/tables.py
+++ db/tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Table, Column, Integer, String, Text, ForeignKey
-from sqlalchemy.dialects.postgresql import MACADDR
+from sqlalchemy.dialects.postgresql import MACADDR, TIMESTAMP
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -28,5 +28,28 @@ class Cell(Base):
     def __repr__(self):
         r = f"""Cell(Id={self.Id!r}, Name={self.Name!r},
         Location={self.Location!r}"""
+
+        return r
+
+class PowerData(Base):
+    __tablename__ = "PowerData"
+
+    Id = Column(Integer, primary_key=True)
+    RocketLogger_Id = Column(Integer, ForeignKey("RocketLoggers.Id"),
+                             nullable=False)
+    Cell_Id = Column(Integer, ForeignKey("Cells.Id"), nullable=False)
+    Timestamp = Column(TIMESTAMP)
+    Current = Column(Integer)
+    Voltage = Column(Integer)
+
+    RocketLogger = relationship("RocketLoggers",
+                                foreign_keys="RocketLoggers.Id")
+    Cell = relationship("Cells", foreign_keys="Cells.Id")
+
+    def __repr__(self):
+        r = f"""PowerData(Id={self.Id!r},
+        RocketLogger_Id={self.RocketLogger_Id!r}, Cell_Id={self.Cell_Id!r},
+        Timestamp={self.Timestamp!r}, Current={self.Current!r},
+        Voltage={self.Voltage!r})"""
 
         return r
