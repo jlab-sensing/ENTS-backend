ca5e89e0379939a743d29eded149a2755ae47341
John Madden <jmadden173@pm.me>
2022-07-26 11:01:27 -0700

    Created seperate add_cell module

--- db/utils/util.py
+++ db/utils/util.py
@@ -9,38 +9,6 @@ from .tables import RocketLogger, Cell, PowerData, TEROSData
 from .conn import engine
 
 
-def add_cell(name, location=None) -> Cell:
-    """Adds a new Cell
-
-    Parameters
-    ----------
-    name : str
-        Name of the Cell
-    location : str
-        Location of the Cell
-
-    Returns
-    -------
-    Reference to newly created Cell object
-    """
-
-    #pdb.set_trace()
-
-
-    with Session(engine) as s:
-        # Create new object
-        c = Cell(
-            name=name,
-            location=location
-        )
-
-        # Add to db
-        s.add(c)
-        s.commit()
-
-        print(c)
-
-
 def add_rl(mac=None, hostname=None) -> RocketLogger:
     """Adds a new rocketlogger
 
