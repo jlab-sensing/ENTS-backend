db43b94556ae1a2f70abbc1b383da3a1c248e4a9
John Madden <jmadden173@pm.me>
2022-08-05 09:56:48 -0700

    Updated import_rl_csv utility

--- dirtviz/db/utils/import_rl_csv.py
+++ dirtviz/db/utils/import_rl_csv.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import Session
 
 from ..conn import engine
 from ..tables import PowerData
+from ..get_or_create import get_or_create_cell, get_or_create_logger
 
 
-def import_rl_csv(path, logger_id, cell1, cell2, batch_size=100000):
+def import_rl_csv(path, logger_name, cell1_name, cell2_name, batch_size=10000):
     """Imports raw RocketLogger data in PowerData table. A logger instance for
     the rokcet locker must be created first.
 
@@ -19,16 +20,17 @@ def import_rl_csv(path, logger_id, cell1, cell2, batch_size=100000):
     ----------
     path : str
         Path to csv file.
-    logger_id : int
-        logger.id for the RocketLogger
-    cell1 : Cell
-        Reference to cell being measured on channel 1.
-    cell2 : Cell
-        Reference to cell being measured on channel 2.
+    logger_name : str
+        Logger name for the Rocketlogger
+    cell1_name : str
+        Name of cell being measured on channel 1.
+    cell2_name : str
+        Name of cell being measured on channel 2.
     """
 
     with open(path, newline='') as csvfile:
         rl_reader = csv.reader(csvfile)
+
 
         # Skip header
         for _ in range(11):
@@ -37,40 +39,46 @@ def import_rl_csv(path, logger_id, cell1, cell2, batch_size=100000):
         count = 0
         tmp = []
 
-        for row in tqdm(rl_reader):
+        with Session(engine) as s:
+            # Get or create objects
+            logger = get_or_create_logger(s, logger_name)
+            cell1 = get_or_create_cell(s, cell1_name)
+            cell2 = get_or_create_cell(s, cell2_name)
 
-            # convert string to timestamp
-            ts = datetime.fromtimestamp(float(row[0]))
+            for row in tqdm(rl_reader):
 
-            pow1 = PowerData(
-                logger_id=logger_id,
-                cell_id=cell1,
-                ts=ts,
-                current=row[4],
-                voltage=row[5],
-            )
+                # convert string to timestamp
+                ts = datetime.fromtimestamp(float(row[0]))
 
-            pow2 = PowerData(
-                logger_id=logger_id,
-                cell_id=cell2,
-                ts=ts,
-                current=row[8],
-                voltage=row[6],
-            )
+                pow1 = PowerData(
+                    logger_id=logger.id,
+                    cell_id=cell1.id,
+                    ts=ts,
+                    current=row[4],
+                    voltage=row[5],
+                )
 
-            tmp.append(pow1)
-            tmp.append(pow2)
+                pow2 = PowerData(
+                    logger_id=logger.id,
+                    cell_id=cell2.id,
+                    ts=ts,
+                    current=row[8],
+                    voltage=row[6],
+                )
 
-            count += 1
-            if (count > batch_size and tmp):
-                with Session(engine) as s:
+                tmp.append(pow1)
+                tmp.append(pow2)
+
+                count += 1
+                if (count > batch_size and tmp):
+                    # Save objects
                     s.bulk_save_objects(tmp)
                     s.commit()
-                count = 0
-                tmp.clear()
+                    # Reset counter
+                    count = 0
+                    tmp.clear()
 
-        # Save remaining objects
-        with Session(engine) as s:
+            # Save remaining objects
             s.bulk_save_objects(tmp)
             s.commit()
 
@@ -79,13 +87,11 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Rocketlogger csv importer utility")
-    parser.add_argument("--batch-size", type=int, default=100000, help="Batch size of inserts")
+    parser.add_argument("--batch-size", type=int, default=10000, help="Batch size of inserts")
     parser.add_argument("path", type=str, help="Name of cell")
-    parser.add_argument("rl", type=int, help="Id of rocketlogger")
-    parser.add_argument("cell1", type=int, help="Id of cell connected to\
-                        channel 1")
-    parser.add_argument("cell2", type=int, help="Id of cell connected to\
-                        channel 2")
+    parser.add_argument("rl", type=str, help="Name of rocketlogger")
+    parser.add_argument("cell1", type=str, help="Name of cell connected to channel 1")
+    parser.add_argument("cell2", type=str, help="Name of cell connected to channel 2")
 
     args = parser.parse_args()
 
