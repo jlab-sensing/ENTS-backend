657f3414fafe1cae13a791aebd1476c5ecacafd5
John Madden <jmadden173@pm.me>
2022-08-09 13:20:02 -0700

    Configured alembic conn to db

--- dirtviz/db/alembic/env.py
+++ dirtviz/db/alembic/env.py
@@ -1,9 +1,14 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from dirtviz.db.tables import Base
+from dirtviz.db.conn import engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +43,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.environ["DB_URL"]
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,11 +62,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
