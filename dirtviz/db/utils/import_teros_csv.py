f4b23c3e7178a64ca7fb7ba11f41316262f56f5c
John Madden <jmadden173@pm.me>
2022-09-06 21:43:58 -0700

    Fixed lint errors with import_teros_csv

--- dirtviz/db/utils/import_teros_csv.py
+++ dirtviz/db/utils/import_teros_csv.py
@@ -1,3 +1,17 @@
+"""TEROS-12 import utility for legacy csv data
+
+Examples
+--------
+Import data for cell1 and cell2 that was logged using rocket1::
+
+    $ python -m dirtviz.db.utils.import_teros_csv data.csv rocket1 cell1 cell2
+
+
+Help prompt for utility::
+
+    $ python -m dirtviz.db.utils.import_teros_csv  -h
+"""
+
 import csv
 from datetime import datetime
 
@@ -8,10 +22,8 @@ from ..conn import engine
 from ..tables import TEROSData
 from ..get_or_create import get_or_create_cell
 
-import pdb
 
-
-def import_teros_csv(path, cell_map, batch_size=10000):
+def import_teros_csv(path, mapping, batch_size=10000):
     """Imports raw TEROS data to table TEROSData
 
     Expects columns in the following format
@@ -26,22 +38,21 @@ def import_teros_csv(path, cell_map, batch_size=10000):
         database.
     """
 
-    with open(path, newline='') as csvfile:
+    with open(path, newline='', encoding='utf-8') as csvfile:
         teros_reader = csv.reader(csvfile)
 
 
         # skip first row
-        teros_reader.__next__()
+        next(teros_reader)
 
-        with Session(engine) as s:
+        with Session(engine) as sess:
             count = 0
             tmp = []
 
             # get cell data
             cell_obj_map = {}
-            for sens_id, cell_name in cell_map.items():
-                c = get_or_create_cell(s, cell_name)
-                cell_obj_map[sens_id] = c
+            for sens_id, cell_name in mapping.items():
+                cell_obj_map[sens_id] = get_or_create_cell(sess, cell_name)
 
             for row in tqdm(teros_reader):
                 # convert string to timestamp
@@ -60,15 +71,15 @@ def import_teros_csv(path, cell_map, batch_size=10000):
                 count += 1
                 if (count > batch_size and tmp):
                     # save objects
-                    s.bulk_save_objects(tmp)
-                    s.commit()
+                    sess.bulk_save_objects(tmp)
+                    sess.commit()
                     # reset counter/tmp array
                     count = 0
                     tmp.clear()
 
             # save remaining objects
-            s.bulk_save_objects(tmp)
-            s.commit()
+            sess.bulk_save_objects(tmp)
+            sess.commit()
 
 
 if __name__ == "__main__":
