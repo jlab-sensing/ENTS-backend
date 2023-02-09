e7f70a459c0b48730c3d6aaa7b41564eb9a96bd8
Adam <adunlop621@gmail.com>
2023-02-09 18:48:32 -0500

    Added date range slider that defaults to an initial query of a week of data, still need to create seperate sliders for each graph and add feature to allow user to type in desired date range

--- dirtviz/main.py
+++ dirtviz/main.py
@@ -18,9 +18,10 @@ Todo
 """
 
 from bokeh.plotting import figure
-from bokeh.models import (ColumnDataSource, LinearAxis, Range1d, Select)
-from bokeh.io import curdoc
+from bokeh.models import (ColumnDataSource, LinearAxis, Range1d, Select, CustomJS)
+from bokeh.io import curdoc, show
 from bokeh import layouts
+from bokeh.models.widgets import DatetimeRangeSlider
 
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -28,6 +29,10 @@ from sqlalchemy import select
 from .db.conn import engine
 from .db.tables import Cell
 from .db.getters import get_power_data, get_teros_data
+
+from datetime import datetime
+
+
 
 # Create empty data sources
 teros_source = ColumnDataSource()
@@ -73,6 +78,30 @@ cell_opts = get_cells()
 cell_select = Select(options=cell_opts)
 update_data(int(cell_opts[0][0]))
 
+#Store source data
+source_data = source.data
+teros_data = teros_source.data
+
+
+#Initialize data range. Timestamps are by the hour,
+#So to set initialize range to all data from the past week, 
+#Set cutoff = -168
+cutoff = -168
+source.data = {
+            'timestamp': source_data["timestamp"][cutoff:],
+            'v': source_data["v"][cutoff:],
+            'i': source_data["i"][cutoff:],
+            'p': source_data["p"][cutoff:]
+        }
+
+teros_source.data = {
+        'timestamp': teros_data["timestamp"][cutoff:],
+        'vwc': teros_data["vwc"][cutoff:],
+        'temp': teros_data["temp"][cutoff:],
+        'ec': teros_data["ec"][cutoff:]
+    }
+
+
 # Plot Power
 power = figure(
     title="Power Measurements",
@@ -113,6 +142,7 @@ teros_temp_vwc = figure(
     y_range=Range1d(start=0, end=100),
     aspect_ratio=2.,
 )
+
 teros_temp_vwc.extra_y_ranges = {"temp": Range1d(start=0, end=40.)}
 teros_temp_vwc.add_layout(LinearAxis(axis_label="Temperature [C]",
                                      y_range_name="temp"), 'right')
@@ -132,14 +162,42 @@ teros_ec = figure(
 teros_ec.line("timestamp", "ec", source=teros_source,
            legend_label="Electrical Conductivity [uS/cm]", color="green")
 
+date_range= DatetimeRangeSlider(
+    title="Date Range",
+    start=source_data["timestamp"][0],
+    end=source_data["timestamp"][-1],
+    value=(source.data["timestamp"][0], source.data["timestamp"][-1]),
+    step=len(source_data["timestamp"]),
+)
+def update_range(attr, old, new):
+    new = [float(i) for i in new]
 
-#date_range= DatetimeRangeSlider(
-#    title="Date Range",
-#    start=rl_data.index[0],
-#    end=rl_data.index[-1],
-#    value=(rl_data.index[0], rl_data.index[-1]),
-#    step=100000,
-#)
+    new_source_data = {
+            'timestamp': [source_data["timestamp"][i] for i in range(len(source_data["timestamp"])) if source_data["timestamp"][i].timestamp()*1000 > new[0] and source_data["timestamp"][i].timestamp()*1000 < new[1]],
+            'v': [source_data["v"][i] for i in range(len(source_data["timestamp"])) if source_data["timestamp"][i].timestamp()*1000 > new[0] and source_data["timestamp"][i].timestamp()*1000 < new[1]],
+            'i': [source_data["i"][i] for i in range(len(source_data["timestamp"])) if source_data["timestamp"][i].timestamp()*1000 > new[0] and source_data["timestamp"][i].timestamp()*1000 < new[1]],
+            'p': [source_data["p"][i] for i in range(len(source_data["timestamp"])) if source_data["timestamp"][i].timestamp()*1000 > new[0] and source_data["timestamp"][i].timestamp()*1000 < new[1]]
+        }
+
+    
+
+    new_teros_data = {
+        'timestamp': [i for i in teros_data["timestamp"] if i.timestamp()*1000 > new[0] and i.timestamp()*1000 < new[1]],
+        'vwc': [teros_data["vwc"][i] for i in range(len(teros_data["timestamp"])) if teros_data["timestamp"][i].timestamp()*1000 > new[0] and teros_data["timestamp"][i].timestamp()*1000 < new[1]] ,
+        'temp': [teros_data["temp"][i] for i in range(len(teros_data["timestamp"])) if teros_data["timestamp"][i].timestamp()*1000 > new[0] and teros_data["timestamp"][i].timestamp()*1000 < new[1]] ,
+        'ec': [teros_data["ec"][i] for i in range(len(teros_data["timestamp"])) if teros_data["timestamp"][i].timestamp()*1000 > new[0] and teros_data["timestamp"][i].timestamp()*1000 < new[1]] 
+    }
+
+
+    source.data = new_source_data
+    teros_source.data = new_teros_data
+
+
+
+
+date_range.on_change('value', update_range)
+
+
 
 # pylint: disable=unused-argument
 def update_cell(attrname, old, new):
@@ -147,13 +205,15 @@ def update_cell(attrname, old, new):
 
     update_data(int(new))
 
-#date_range.on_change('value', update_range)
 cell_select.on_change('value', update_cell)
 
 rl_col = layouts.column(power, vi)
 teros_col = layouts.column(teros_temp_vwc, teros_ec)
 graphs = layouts.row(rl_col, teros_col)
-layout = layouts.column(cell_select, graphs, width=1000)
+layout = layouts.column(cell_select, date_range, graphs, width=1000)
+
+
+
 
 curdoc().add_root(layout)
 curdoc().title = "DirtViz"
--- import_example_data.py
+++ import_example_data.py
@@ -5,12 +5,12 @@ from dirtviz.db.utils.import_teros_csv import import_teros_csv
 
 if __name__ == "__main__":
     print("Importing rocketlogger data")
-    import_rl_csv("data/soil_20220629-214516_8.csv", "examplelogger", "cell1",
+    import_rl_csv("data/soil_20220629-214516_8_long.csv", "examplelogger", "cell1",
                   "cell2")
 
     print("Importing TEROS-12 data")
     import_teros_csv(
-        path="data/TEROSoutput-1656537434-f17.csv",
+        path="data/TEROSoutput-1656537434-f17_long.csv",
         mapping={
             "1": "cell1",
             "3": "cell2"
