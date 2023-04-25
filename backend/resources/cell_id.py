8fe746048594de4660c747e3e6ec0e54c301a689
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-04-24 21:38:06 -0700

    added new api resources

---
+++ backend/resources/cell_id.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource
+import json
+from json import JSONEncoder
+import decimal
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from ..db.conn import engine
+from ..db.tables import Cell
+from ..db.getters import get_power_data, get_teros_data
+
+
+class Cell_Id(Resource):
+    def get(self):
+        with Session(engine) as sess:
+            # Create cell select widget
+            stmt = select(Cell).order_by(Cell.name)
+            opts = [(str(c.id), repr(c)) for c in sess.scalars(stmt)]
+        return json.dumps(opts)
+
+    def post(self):
+        pass
