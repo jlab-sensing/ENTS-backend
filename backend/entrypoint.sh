29e67336bb990ae163d882573cc8152cfad85f62
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-05-26 14:44:56 -0700

    added entrypoint for migrations

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# apply migrations
 flask --app backend.api db migrate -d ./backend/api/migrations
 flask --app backend.api db upgrade head -d ./backend/api/migrations
-flask --app backend.api --debug run -p 8000
+
+# run in dev or prod
+while getopts 'dp' FLAG
+do
+    case "$FLAG" in
+        d) flask --app backend.api --debug run -p 8000;;
+        p) gunicorn -b :8000 backend.wsgi:handler;;
+    esac
+done
+
+
+
