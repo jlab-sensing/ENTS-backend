b0d78ce0c90e2de1898bb49aa3573a93be5504f4
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-07-14 14:57:39 -0700

    added pool pre ping to handle db disconnects

--- backend/api/config.py
+++ backend/api/config.py
@@ -7,9 +7,11 @@ Environment variables for flask application
 import os
 from .conn import dburl
 
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
     DEBUG = True
     CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = dburl
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
