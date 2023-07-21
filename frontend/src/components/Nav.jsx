b4bfef57b482ffc66b0ef5c952359d83393ed41f
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-07-20 20:45:54 -0700

    WIP: added nav and more formatting

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,96 @@
+import { React, useState } from "react";
+import {
+  AppBar,
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Box,
+  Typography,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import DvIcon from "./DvIcon";
+import { useNavigate } from "react-router-dom";
+
+function Nav() {
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{ bgcolor: "transparent", pl: "5%", pr: "5%" }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <DvIcon></DvIcon>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorElNav}
+              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem key="Home" onClick={() => navigate("/")}>
+                <Typography textAlign="center">Home</Typography>
+              </MenuItem>
+              <MenuItem key="Dashboard" onClick={() => navigate("/dashboard")}>
+                <Typography textAlign="center">Dashboard</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button
+              key="Home"
+              onClick={() => navigate("/")}
+              sx={{ my: 2, color: "black", display: "block" }}
+            >
+              Home
+            </Button>
+            <Button
+              key="Dashboard"
+              onClick={() => navigate("/dashboard")}
+              sx={{ my: 2, color: "black", display: "block" }}
+            >
+              Dashboard
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default Nav;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,67 @@
 import { React } from "react";
 import Nav from "../../components/Nav";
+import { Container, Box, Button } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import chartsHome from "../../assets/chartsHome.png";
 
 function Home() {
   return (
-    <>
+    <Container
+      disableGutters={true}
+      maxWidth={false}
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#DAD7CD",
+      }}
+    >
       <Nav></Nav>
-    </>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          flexGrow: 1,
+        }}
+      >
+        <Box
+          maxWidth="sm"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <h1>A data visualization application for xyz</h1>
+          <p>
+            sub-header to explain use case.... Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. In aliquam sem fringilla ut. Sed
+            egestas egestas fringilla phasellus faucibus scelerisque.
+          </p>
+          <Box maxWidth="sm" sx={{ display: "flex", flexDirection: "row" }}>
+            <Button>Checkout live data</Button>
+            <Button>Learn More</Button>
+          </Box>
+        </Box>
+        <Box>
+          <img src="chartsHome"></img>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          paddingBottom: "5%",
+          justifySelf: "flex-end",
+        }}
+      >
+        <ExpandMoreIcon></ExpandMoreIcon>
+      </Box>
+    </Container>
   );
 }
 
