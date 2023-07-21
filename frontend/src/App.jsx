d15acdc0efd9eeb7255e95f9f20093948afbc023
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-07-20 19:18:09 -0700

    added skeleton for nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { React } from "react";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import { React } from "react";
+import Nav from "../../components/Nav";
+
+function Home() {
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  );
+}
+
+export default Home;
