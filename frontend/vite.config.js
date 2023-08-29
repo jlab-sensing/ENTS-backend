f0d8fe264a73e93932b88c3afac63e9920285c17
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-08-28 18:11:58 -0700

    init linters

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -45,6 +46,6 @@ export default defineConfig(({ mode }) => {
       },
       outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [react(), eslint()],
   };
 });
