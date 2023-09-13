8d4da0c12f733383c1399182a948b5e11cb75fc1
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-09-12 18:55:24 -0700

    linted

--- frontend/src/pages/dashboard/Dashboard.test.jsx
+++ frontend/src/pages/dashboard/Dashboard.test.jsx
@@ -1,20 +1,13 @@
-import { describe, test, expect, vi } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Dashboard from './Dashboard';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CellSelect from './components/CellSelect';
 
 const queryClient = new QueryClient();
 
-const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-
 describe('Loading dashboard', () => {
-  //   test('Renders Dashbaord', async () => {
-  //     const dash = render(<Dashboard />);
-  //     expect(await dash.findByText('Cell')).toBeInTheDocument();
-  //   });
   test('Fetch and display cells to add', async () => {
     const user = userEvent.setup();
     render(
