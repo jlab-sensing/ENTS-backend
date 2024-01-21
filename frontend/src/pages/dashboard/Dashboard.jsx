c595b9ec9ca8498be770fc83fe75f2b7e608427d
Aaron Wu <12989649+aaron-wu1@users.noreply.github.com>
2024-01-20 21:31:43 -0800

    moved leaf chart down (#149)

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -16,44 +16,54 @@ function Dashboard() {
   const [selectedCells, setSelectedCells] = useState([]);
 
   return (
-    <Stack
-      direction='column'
-      divider={<Divider orientation='horizontal' flexItem />}
-      justifyContent='spaced-evently'
-      sx={{ height: '100vh', boxSizing: 'border-box' }}
-    >
-      <Stack direction='row' alignItems='center' justifyContent='space-evenly' sx={{ p: 2 }} flex>
-        <BackBtn />
-        <CellSelect selectedCells={selectedCells} setSelectedCells={setSelectedCells} />
-        <Box display='flex' justifyContent='center' alignItems='center'>
-          <DateRangeSel
+    <>
+      <Stack
+        direction='column'
+        divider={<Divider orientation='horizontal' flexItem />}
+        justifyContent='spaced-evently'
+        sx={{ height: '100vh', boxSizing: 'border-box' }}
+      >
+        <Stack direction='row' alignItems='center' justifyContent='space-evenly' sx={{ p: 2 }} flex>
+          <BackBtn />
+          <CellSelect selectedCells={selectedCells} setSelectedCells={setSelectedCells} />
+          <Box display='flex' justifyContent='center' alignItems='center'>
+            <DateRangeSel
+              startDate={startDate}
+              endDate={endDate}
+              setStartDate={setStartDate}
+              setEndDate={setEndDate}
+            ></DateRangeSel>
+          </Box>
+          <DownloadBtn
+            disabled={dBtnDisabled}
+            setDBtnDisabled={setDBtnDisabled}
+            cells={selectedCells}
             startDate={startDate}
             endDate={endDate}
-            setStartDate={setStartDate}
-            setEndDate={setEndDate}
-          ></DateRangeSel>
-        </Box>
-        <DownloadBtn
-          disabled={dBtnDisabled}
-          setDBtnDisabled={setDBtnDisabled}
-          cells={selectedCells}
-          startDate={startDate}
-          endDate={endDate}
-        />
+          />
+        </Stack>
+        <Grid
+          container
+          spacing={3}
+          sx={{ height: '100%', width: '100%', p: 2 }}
+          alignItems='center'
+          justifyContent='space-evenly'
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          <PowerCharts cells={selectedCells} startDate={startDate} endDate={endDate} />
+          <TerosCharts cells={selectedCells} startDate={startDate} endDate={endDate} />
+        </Grid>
       </Stack>
-      <Grid
-        container
-        spacing={3}
-        sx={{ height: '100%', width: '100%', p: 2 }}
-        alignItems='center'
-        justifyContent='space-evenly'
-        columns={{ xs: 4, sm: 8, md: 12 }}
+
+      <Stack
+        direction='column'
+        divider={<Divider orientation='horizontal' flexItem />}
+        justifyContent='spaced-evently'
+        sx={{ height: '100vh', boxSizing: 'border-box' }}
       >
-        <PowerCharts cells={selectedCells} startDate={startDate} endDate={endDate} />
-        <TerosCharts cells={selectedCells} startDate={startDate} endDate={endDate} />
-      </Grid>
-      <SensorChart cells={selectedCells} startDate={startDate} endDate={endDate} />
-    </Stack>
+        <SensorChart cells={selectedCells} startDate={startDate} endDate={endDate} />
+      </Stack>
+    </>
   );
 }
 
