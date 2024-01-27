6c1ea19157cd2f63b019e1e33f78a57b0442d8b0
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-01-26 21:34:38 -0800

    added refresh hooks + migrating to axiosclient

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const RESOURCES_URL = `${process.env.PUBLIC_URL}/api`;
+
+export default axiosClient = axios.create({
+  baseURL: RESOURCES_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: RESOURCES_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
---
+++ frontend/src/auth/hooks/useAxiosPrivate.js
@@ -0,0 +1,12 @@
+import { axiosPrivate } from '../api/axios';
+import { useEffect } from 'react';
+import useRefreshToken from './useRefreshToken';
+import useAuth from './useAuth';
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+  useEffect(() => {
+    //TODO: finish writing interceptors to refresh
+  });
+};
