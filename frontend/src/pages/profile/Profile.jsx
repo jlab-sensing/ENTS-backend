f3e3aa1c63a825b2040534a652a9ea732f146ad9
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-04-10 12:38:15 -0700

    starting code to get logged in user

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,10 +1,14 @@
 import { React } from 'react';
 import AddCellModal from './components/AddCellModal';
+import useAuth from '../../auth/hooks/useAuth';
+import Nav from '../../components/Nav';
 
 function Profile() {
+  const { user, loggedIn } = useAuth();
   return (
     <>
-      <AddCellModal />
+      <Nav />
+      {loggedIn === true ? <AddCellModal /> : <h1>NOT LOGGED IN!</h1>}
     </>
   );
 }
