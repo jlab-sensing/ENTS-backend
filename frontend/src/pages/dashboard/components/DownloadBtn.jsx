936e545879e7ede3242520dc6c9c7b96cdd5d6c0
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-05-30 09:19:15 -0700

    refactored csv end point to stream blobs

--- frontend/src/pages/dashboard/components/DownloadBtn.jsx
+++ frontend/src/pages/dashboard/components/DownloadBtn.jsx
@@ -7,96 +7,104 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 function DownloadBtn({ cells, startDate, endDate }) {
   // Get QueryClient from the context
-  const queryClient = useQueryClient()
+  // const queryClient = useQueryClient()
 
-  useEffect(()=>{queryClient.invalidateQueries({ queryKey: ['cells'] })}, [cells, startDate, endDate, queryClient])
-
-
-  const { isLoading, isError, data, error } = 
-    useQuery({
-      queryKey: ['cells'],
-      queryFn: () => getCellData(cells.map((c)=>c.id), 'none', startDate, endDate),
-      enabled: !!(Array.isArray(cells) && cells.length),
-      refetchOnWindowFocus: false,
-    })
+  // useEffect(() => { queryClient.invalidateQueries({ queryKey: ['cells'] }) }, [cells, startDate, endDate, queryClient])
 
 
-  const downloadFile = ({ data, fileName, fileType }) => {
-    const blob = new Blob([data], { type: fileType });
+  // const { isLoading, isError, data, error, isFetching } =
+  //   useQuery({
+  //     queryKey: ['cells'],
+  //     queryFn: () => getCellData(cells.map((c) => c.id), 'none', startDate, endDate),
+  //     enabled: !!(Array.isArray(cells) && cells.length),
+  //   refetchOnWindowFocus: false,
+  //   })
 
-    const a = document.createElement('a');
-    a.download = fileName;
-    a.href = window.URL.createObjectURL(blob);
-    const clickEvt = new MouseEvent('click', {
-      view: window,
-      bubbles: true,
-      cancelable: true,
-    });
-    a.dispatchEvent(clickEvt);
-    a.remove();
+
+  const downloadFile = () => {
+    for (const { id, name } of cells) {
+      const fileName = name + '.csv'
+      const resample = 'none'
+      getCellData(id, resample, startDate, endDate).then((data) => {
+        const blob = data
+        const a = document.createElement('a');
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent('click', {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+      })
+    }
   };
   /** 
     exports cell data from json obj to csv.
     runs with static number of headers
   **/
   const exportToCsv = (e) => {
-    e.preventDefault();
-    // Note: timestamp string slices of "Wed," in "Wed, 29 Jun 2022 14:00:00 GMT"
-    const resample = 'none';
-    for (const { id, name } of cells) {
-      getCellData(id, resample, startDate, endDate).then((data) => {
-        downloadFile({
-          data: [
-            [
-              'timestamp',
-              'Voltage (mV)',
-              'Current (uA)',
-              'Power (uW)',
-              'EC (uS/cm)',
-              'VWC (%)',
-              'Raw VWC',
-              'Temperature (C)',
-              'leaf_voltage (V)',
-            ],
-            ...data.map((point) => [
-              // point.timestamp,
-              DateTime.fromHTTP(point.timestamp.slice()), // to get rid of str day format
-              point.v,
-              point.i,
-              point.p,
-              point.ec,
-              point.vwc,
-              point.raw_vwc,
-              point.temp,
-              point.data,
-            ]),
-          ]
-            .map((e) => e.join(','))
-            .join('\n'),
-          fileName: name + '.csv',
-          fileType: 'text/csv',
-        });
-      });
-    }
+    downloadFile();
+  //   e.preventDefault();
+  //   // Note: timestamp string slices of "Wed," in "Wed, 29 Jun 2022 14:00:00 GMT"
+  //   const resample = 'none';
+  //   for (const { id, name } of cells) {
+  //     getCellData(id, resample, startDate, endDate).then((data) => {
+  //       downloadFile({
+  //         data: [
+  //           [
+  //             'timestamp',
+  //             'Voltage (mV)',
+  //             'Current (uA)',
+  //             'Power (uW)',
+  //             'EC (uS/cm)',
+  //             'VWC (%)',
+  //             'Raw VWC',
+  //             'Temperature (C)',
+  //             'leaf_voltage (V)',
+  //           ],
+  //           ...data.map((point) => [
+  //             // point.timestamp,
+  //             DateTime.fromHTTP(point.timestamp.slice()), // to get rid of str day format
+  //             point.v,
+  //             point.i,
+  //             point.p,
+  //             point.ec,
+  //             point.vwc,
+  //             point.raw_vwc,
+  //             point.temp,
+  //             point.data,
+  //           ]),
+  //         ]
+  //           .map((e) => e.join(','))
+  //           .join('\n'),
+  //         fileName: name + '.csv',
+  //         fileType: 'text/csv',
+  //       });
+  //     });
+  //   }
   };
   return (
     <div className='DownloadBtn'>
-       {data ? (
+      {/* {data ? (
         <Button disabled={false} variant='outlined' onClick={exportToCsv}>Export to CSV
         </Button>
       ) : isError ? (
         <Button disabled={true} variant='outlined' onClick={exportToCsv}>ERROR: ${error}
         </Button>
-      ) : isLoading ? (
+      ) : isLoading || isFetching ? (
         <Button disabled={true} variant='outlined' onClick={exportToCsv}>LOADING...
         </Button>
       ) : (!Array.isArray(cells) && cells.length) ? (
         <Button disabled={true} variant='outlined' onClick={exportToCsv}>Select a cell
         </Button>
-      ) :(
+      ) : (
         <Button disabled={true} variant='outlined' onClick={exportToCsv}>No data
-        </Button> 
-      )}
+        </Button>
+      )} */}
+        <Button disabled={false} variant='outlined' onClick={exportToCsv}>Export to CSV
+        </Button>
     </div>
   );
 }
