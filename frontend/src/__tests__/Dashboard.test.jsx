e26454aada446a52683d7773559fcf2b6f6edc66
aleclevy <aleclevy3@gmail.com>
2024-08-06 15:02:39 -0700

    Added Unit Tests

--- frontend/src/__tests__/Dashboard.test.jsx
+++ frontend/src/__tests__/Dashboard.test.jsx
@@ -4,10 +4,13 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CellSelect from '../pages/dashboard/components/CellSelect';
+import CopyLinkBtn from '../pages/dashboard/components/CopyLinkBtn';
+import { DateTime } from 'luxon';
 import PropTypes from 'prop-types';
 
 const queryClient = new QueryClient();
 const mockedSetSelectedCells = vi.fn();
+const DateTimeNow = DateTime.now();
 
 const MockCellSelect = ({ selectedCells, setSelectedCells }) => {
   return (
@@ -29,7 +32,11 @@ describe('Loading dashboard', () => {
     const cellSelectElement = await screen.findByText('select-cell');
     expect(cellSelectElement).toBeInTheDocument();
   });
-
+  it('should load copy link button', async () => {
+    render(<CopyLinkBtn startDate={DateTimeNow} endDate={DateTimeNow} selectedCells={[]} />);
+    const copyLinkButton = await screen.findByLabelText('Copy Link');
+    expect(copyLinkButton).toBeInTheDocument();
+  });
   it('should display mocked cell names when dropdown is open', async () => {
     const user = userEvent.setup();
     render(<MockCellSelect selectedCells={[]} setSelectedCells={mockedSetSelectedCells} />);
@@ -37,5 +44,75 @@ describe('Loading dashboard', () => {
     await user.click(cellDropDownButton);
     expect(await screen.findByText('test_cell_1')).toBeInTheDocument();
     expect(await screen.findByText('test_cell_2')).toBeInTheDocument();
+  });
+});
+
+describe('Testing copy functionality', () => {
+  it('should copy a URL with the correct cellID QueryParam of 1,2', async () => {
+    const writeTextMock = vi.fn();
+    vi.spyOn(navigator.clipboard, 'writeText').mockImplementation(writeTextMock);
+
+    const user = userEvent.setup();
+    const selectedCells = [{ id: 1 }, { id: 2 }];
+    render(<CopyLinkBtn startDate={DateTimeNow} endDate={DateTimeNow} selectedCells={selectedCells} />);
+    const copyLinkButton = screen.getByLabelText('Copy Link');
+    await user.click(copyLinkButton);
+    const copiedText = `http://localhost:3000/dashboard?cell_id=${selectedCells
+      .map((cell) => cell.id)
+      .join(',')}&startDate=${DateTimeNow.toISO()}&endDate=${DateTimeNow.toISO()}`;
+
+    expect(writeTextMock).toHaveBeenCalledWith(copiedText);
+
+    vi.restoreAllMocks();
+  });
+
+  it('should copy a URL with the correct cellID QueryParam of 12', async () => {
+    const writeTextMock = vi.fn();
+    vi.spyOn(navigator.clipboard, 'writeText').mockImplementation(writeTextMock);
+
+    const user = userEvent.setup();
+    const selectedCells = [{ id: 12 }];
+    render(<CopyLinkBtn startDate={DateTimeNow} endDate={DateTimeNow} selectedCells={selectedCells} />);
+    const copyLinkButton = screen.getByLabelText('Copy Link');
+    await user.click(copyLinkButton);
+    const copiedText = `http://localhost:3000/dashboard?cell_id=${selectedCells
+      .map((cell) => cell.id)
+      .join(',')}&startDate=${DateTimeNow.toISO()}&endDate=${DateTimeNow.toISO()}`;
+
+    expect(writeTextMock).toHaveBeenCalledWith(copiedText);
+
+    vi.restoreAllMocks();
+  });
+
+  it('should copy a URL with the correct startDate QueryParam', async () => {
+    const writeTextMock = vi.fn();
+    vi.spyOn(navigator.clipboard, 'writeText').mockImplementation(writeTextMock);
+    const startDate = DateTime.now().minus({ days: 14 });
+
+    const user = userEvent.setup();
+    render(<CopyLinkBtn startDate={startDate} endDate={DateTimeNow} selectedCells={[]} />);
+    const copyLinkButton = screen.getByLabelText('Copy Link');
+    await user.click(copyLinkButton);
+    const copiedText = `http://localhost:3000/dashboard?cell_id=&startDate=${startDate.toISO()}&endDate=${DateTimeNow.toISO()}`;
+
+    expect(writeTextMock).toHaveBeenCalledWith(copiedText);
+
+    vi.restoreAllMocks();
+  });
+
+  it('should copy a URL with the correct endDate QueryParam', async () => {
+    const writeTextMock = vi.fn();
+    vi.spyOn(navigator.clipboard, 'writeText').mockImplementation(writeTextMock);
+    const endDate = DateTime.now().minus({ days: 14 });
+
+    const user = userEvent.setup();
+    render(<CopyLinkBtn startDate={DateTimeNow} endDate={endDate} selectedCells={[]} />);
+    const copyLinkButton = screen.getByLabelText('Copy Link');
+    await user.click(copyLinkButton);
+    const copiedText = `http://localhost:3000/dashboard?cell_id=&startDate=${DateTimeNow}&endDate=${endDate.toISO()}`;
+
+    expect(writeTextMock).toHaveBeenCalledWith(copiedText);
+
+    vi.restoreAllMocks();
   });
 });
--- frontend/src/pages/dashboard/components/CopyLinkBtn.jsx
+++ frontend/src/pages/dashboard/components/CopyLinkBtn.jsx
@@ -1,43 +1,44 @@
 import { React } from 'react';
-import { Button, Box, Tooltip} from '@mui/material';
+import { Button, Box, Tooltip } from '@mui/material';
 import share from '../../../assets/share.svg';
 import { PropTypes } from 'prop-types';
 
 export default function CopyLinkBtn({ startDate, endDate, selectedCells }) {
-    const copyLink = () => {
-        const text = `${window.location.origin}/dashboard?cell_id=${
-            selectedCells.map((cell) => cell.id).join(",")}
-            &startDate=${startDate.toISO()}&endDate=${endDate.toISO()}`;
-        navigator.clipboard.writeText(text);
-    };
+  const copyLink = () => {
+    const text = `${window.location.origin}/dashboard?cell_id=${selectedCells
+      .map((cell) => cell.id)
+      .join(',')}&startDate=${startDate.toISO()}&endDate=${endDate.toISO()}`;
 
-    return (
-        <Tooltip
-        title='Copy Link'
-        placement='bottom'
-        disableInteractive
-        slotProps={{
-          popper: {
-            modifiers: [
-              {
-                name: 'offset',
-                options: {
-                  offset: [0, -11],
-                },
+    navigator.clipboard.writeText(text);
+  };
+
+  return (
+    <Tooltip
+      title='Copy Link'
+      placement='bottom'
+      disableInteractive
+      slotProps={{
+        popper: {
+          modifiers: [
+            {
+              name: 'offset',
+              options: {
+                offset: [0, -11],
               },
-            ],
-          },
-        }}
-      >
-        <Button variant="outlined" onClick={copyLink}>
-            <Box component="img" src={share} />
-        </Button>
-        </Tooltip>
-    );
+            },
+          ],
+        },
+      }}
+    >
+      <Button variant='outlined' onClick={copyLink}>
+        <Box component='img' src={share} />
+      </Button>
+    </Tooltip>
+  );
 }
 
 CopyLinkBtn.propTypes = {
-    startDate: PropTypes.object.isRequired,
-    endDate: PropTypes.object.isRequired,
-    selectedCells: PropTypes.array.isRequired,
+  startDate: PropTypes.object.isRequired,
+  endDate: PropTypes.object.isRequired,
+  selectedCells: PropTypes.array.isRequired,
 };
