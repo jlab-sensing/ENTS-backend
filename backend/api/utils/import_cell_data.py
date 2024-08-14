6d162e6fdab79d670076adb5cfea86628f92c02c
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-08-14 14:00:49 -0700

    moved get_or_create to util, updated cell init

--- backend/api/utils/import_cell_data.py
+++ backend/api/utils/import_cell_data.py
@@ -21,7 +21,7 @@ from sqlalchemy.orm import Session
 from ..conn import engine
 from ..models.power_data import PowerData
 from ..models.teros_data import TEROSData
-from ..get_or_create import get_or_create_cell, get_or_create_logger
+from .get_or_create import get_or_create_cell, get_or_create_logger
 
 
 def import_cell_data(path, logger_name, cell_name, batch_size=10000):
