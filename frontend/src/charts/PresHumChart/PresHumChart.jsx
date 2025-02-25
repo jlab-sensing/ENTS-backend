4ca0f19f0600f0a4e9e8533918336b479e23ab56
aleclevy <aleclevy3@gmail.com>
2025-02-25 15:24:51 -0800

    initial commit for Teros and BME graphs

---
+++ frontend/src/charts/PresHumChart/PresHumChart.jsx
@@ -0,0 +1,74 @@
+import { React } from 'react';
+import 'chartjs-adapter-luxon';
+import PropTypes from 'prop-types';
+import ChartWrapper from './ChartWrapper';
+import { chartPlugins } from './plugins';
+import { getMaxAxisAndStepValues } from './alignAxis';
+
+export default function PresHumchart({ data }) {
+  const { leftYMax, leftYStep } = getMaxAxisAndStepValues(data.datasets, [], 8, .2);
+  const chartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      x: {
+        position: 'bottom',
+        title: {
+          display: true,
+          text: 'Time',
+        },
+        type: 'time',
+        ticks: {
+          autoSkip: false,
+          autoSkipPadding: 50,
+          maxRotation: 0,
+          major: {
+            enabled: true,
+          },
+        },
+        time: {
+          displayFormats: {
+            hour: 'hh:mm a',
+            day: 'D',
+          },
+        },
+      },
+      pressure: {
+        position: 'left',
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Pressure (hPa)',
+        },
+        ticks: {
+          beginAtZero: true,
+          stepSize: leftYStep,
+        },
+        min: 0,
+        max: leftYMax,
+      },
+      humidity: {
+        position: 'right',
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Humidity (% RH)',
+        },
+        ticks: {
+          beginAtZero: true,
+          stepSize: leftYStep,
+        },
+        min: 0,
+        max: leftYMax,
+    },
+    },
+    plugins: structuredClone(chartPlugins),
+  };
+
+  return <ChartWrapper id='presHum' data={data} options={chartOptions} />;
+}
+
+SensorChartTemplate.propTypes = {
+  id: PropTypes.string,
+  data: PropTypes.object,
+};
---
+++ frontend/src/charts/SoilPotChart/SoilPotChart.jsx
@@ -0,0 +1,60 @@
+import { React } from 'react';
+import 'chartjs-adapter-luxon';
+import PropTypes from 'prop-types';
+import ChartWrapper from './ChartWrapper';
+import { chartPlugins } from './plugins';
+import { getMaxAxisAndStepValues } from './alignAxis';
+
+export default function SoilPotChart({ data }) {
+  const { leftYMax, leftYStep } = getMaxAxisAndStepValues(data.datasets, [], 8, .2);
+  const chartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      x: {
+        position: 'bottom',
+        title: {
+          display: true,
+          text: 'Time',
+        },
+        type: 'time',
+        ticks: {
+          autoSkip: false,
+          autoSkipPadding: 50,
+          maxRotation: 0,
+          major: {
+            enabled: true,
+          },
+        },
+        time: {
+          displayFormats: {
+            hour: 'hh:mm a',
+            day: 'D',
+          },
+        },
+      },
+      y: {
+        position: 'left',
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Soil Potential (kPA)',
+        },
+        ticks: {
+          beginAtZero: true,
+          stepSize: leftYStep,
+        },
+        min: 0,
+        max: leftYMax,
+      },
+    },
+    plugins: structuredClone(chartPlugins),
+  };
+
+  return <ChartWrapper id='sPot' data={data} options={chartOptions} />;
+}
+
+SensorChartTemplate.propTypes = {
+  id: PropTypes.string,
+  data: PropTypes.object,
+};
