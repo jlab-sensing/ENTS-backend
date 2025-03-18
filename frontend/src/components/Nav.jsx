592d6520cd810a2e5b73dff4d0cefb1996721cd7
Devansh kukreja <117558669+devansh1401@users.noreply.github.com>
2025-03-18 17:21:05 +0530

    fix: small fix

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,4 +1,3 @@
-// updated nav.jsx
 import MenuIcon from '@mui/icons-material/Menu';
 import { AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
