04f8192f7a069b3757c27c9f23eab9167e8af992
Stainman Hu <stanleyhuofficial@gmail.com>
2025-04-19 13:17:01 -0700

    In the process of converting from css to mui box sx style.

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Contact.css'
+import { Box } from '@mui/material';
 import Swal from 'sweetalert2'
+import Nav from '../../components/Nav';
 
 const Contact = () => {
    const onSubmit = async (event) => {
@@ -30,27 +32,68 @@ const Contact = () => {
       }
    };
   
+   const styles = {
+      leafletContainer: {
+         width: '100%',
+         height: '100vh',
+      },
+   };
 
   return (
-    <section className="contact">
-      <form onSubmit={onSubmit}>
-         <h2>Contact Form</h2>
-         <div className="input-box">
-            <label>Full Name</label>
-            <input type="text" className="field" placeholder="Enter your name" name="name" required />
-         </div>
-         <div className="input-box">
-            <label>Email Address</label>
-            <input type="email" className="field" placeholder="Enter your email" name="email" required />
-         </div>
-         <div className="input-box">
-            <label>Your Message</label>
-            <textarea name="message" type="Your Message" className="field mess" placeholder="Enter your message"  required ></textarea>
-         </div>
-         <button type="submit">Send Message</button>
-      </form>
-    </section>
-  )
+   
+   <Box
+      sx={{
+         height: '100vh',
+         width: '100%',
+         position: 'relative',
+         scrollSnapAlign: 'center',
+         scrollSnapStop: 'always',
+         display: 'flex',
+         flexDirection: 'column',
+         justifyContent: 'space-between',
+         backgroundColor: '#DAD7CD',
+      }}
+   >
+      <Nav />
+      <Box
+         component = {'section'}
+         sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            backgroundColor: '#00cc00'
+         }}
+      >
+         <Box
+            component = {'form'}
+            sx={{marginTop: '20px'}}
+            onSubmit={onSubmit}
+         >
+            <Box
+               component = {'h2'}
+               sx = {{
+                  fontSize: '30px',
+                  textAlign: 'center'
+               }}   
+            >Contact Form</Box>
+            <Box s={{marginTop: '20px'}}>
+               <label>Full Name</label>
+               <input type="text" className="field" placeholder="Enter your name" name="name" required />
+            </Box>
+            <div className="input-box">
+               <label>Email Address</label>
+               <input type="email" className="field" placeholder="Enter your email" name="email" required />
+            </div>
+            <div className="input-box">
+               <label>Your Message</label>
+               <textarea name="message" type="Your Message" className="field mess" placeholder="Enter your message"  required ></textarea>
+            </div>
+            <button type="submit">Send Message</button>
+         </Box>
+      </Box>
+   </Box>
+  );
 }
 
 export default Contact
