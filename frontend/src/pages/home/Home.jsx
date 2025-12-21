683c15d5b429992468b1002c47ae95ef5861d62a
aleclevy <aleclevy3@gmail.com>
2025-12-21 15:36:19 -0800

    linted

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import { React } from 'react';
 import { Container } from '@mui/material';
 import About from './About';
 import Intro from './Intro';
-import TopNav from '../../components/TopNav.jsx';
 
 
 function Home() {
