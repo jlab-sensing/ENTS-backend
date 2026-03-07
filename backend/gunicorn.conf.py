b77b7021dd86a74935fc56beb3811e85306118c6
Abhinav Mishra <abhinav15102003@gmail.com>
2026-03-07 20:06:36 +0530

    fix(docker): route gunicorn worker temp to /dev/shm to resolve [Errno 13] permission denied

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -10,6 +10,7 @@ bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connections = 1000
+worker_tmp_dir = "/dev/shm"
 
 # Additional settings for Socket.IO
 keepalive = 2
