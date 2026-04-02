6a701c02f8644aa55a0ec894a5a4725fabb776a5
Alec Levy <alevy1@ucsc.edu>
2026-04-02 15:26:59 -0700

    changed gunicorn workers to 2

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -7,7 +7,8 @@ except ImportError:
 import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2
+# multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connections = 1000
 
